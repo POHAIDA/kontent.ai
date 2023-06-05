4ab7a0035d131ea488d5dbb880adc9e503c0f76f
viktor <vitja15041504@gmail.com>
2023-06-05 13:54:17 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+
 const index = () => {
-    return (
-      <div>
-        <h1>hello</h1>
-      </div>
-    );
-  };
-  
-  export default index;
-  
+  return (  
+    <div>
+      <Link href="/page">builder info</Link>
+    </div>
+  )
+}
+
+export default index
